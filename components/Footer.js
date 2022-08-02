c35d1e536563d6fa64baf951c27b49e500e417b6
Emily Terranova <emilyeatscode@gmail.com>
2022-08-01 23:07:21 -0400

    Align footer items and add base footer data and structure

--- components/Footer.js
+++ components/Footer.js
@@ -5,30 +5,32 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <section>
-          <span className={styles.logo}>
-            <img
-              src="/keystonelogo.svg"
-              alt="Keystone Logo"
-              className={styles.logo}
-            />
-          </span>
-        </section>
-        <section className={styles.column}>
-          <h4>Location</h4>
-          {siteMetadata.address}
-          <h4>Call Us</h4>
-          {siteMetadata.phoneNumber}
-        </section>
-        <section className={styles.column}>
-          <h4>Location</h4>
-          {siteMetadata.address}
-          <h4>Call Us</h4>
-          {siteMetadata.phoneNumber}
-        </section>
-        <section>
-          <h4>hours</h4>
-        </section>
+        <div className={styles.footerColumns}>
+          <section className={styles.column}>
+            <span className={styles.logo}>
+              <img
+                src="/keystonelogo.svg"
+                alt="Keystone Logo"
+                className={styles.logo}
+              />
+            </span>
+          </section>
+          <section className={styles.column}>
+            <h4>Location</h4>
+            {siteMetadata.address}
+            <h4>Call Us</h4>
+            {siteMetadata.phoneNumber}
+          </section>
+          <section className={styles.column}>
+            <h4>Location</h4>
+            {siteMetadata.address}
+            <h4>Call Us</h4>
+            {siteMetadata.phoneNumber}
+          </section>
+          <section className={styles.column}>
+            <h4>hours</h4>
+          </section>
+        </div>
         <div className={styles.copyright}>
           (c) year {siteMetadata.companyName} {siteMetadata.copyright}
         </div>
