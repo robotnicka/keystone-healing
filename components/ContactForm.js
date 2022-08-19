1eadcad1c0fb4381e51de3c7aec8372b7613b908
Emily Terranova <emilyeatscode@gmail.com>
2022-08-18 20:17:01 -0400

    Add favicons and contact form, marquee layout

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,53 +3,58 @@ import styles from './ContactForm.module.scss';
 export function ContactForm({ item }) {
   return (
     <>
-      <h2>{item.title}</h2>
-      <p>{item.content}</p>
-      <form
-        className={styles.form}
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        name="feedback"
-        method="POST"
-        action="/success"
-        id="contact-us"
-      >
-        <input type="hidden" name="form-name" value="feedback" />
-        <p className={styles.hidden}>
-          <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-          </label>
-        </p>
-
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          className={styles['form-field']}
-          type="text"
-          name="name"
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          className={styles['form-field']}
-          type="email"
-          name="email"
-          required
-        />
-
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea
-          id="feedback"
-          className={styles['form-field']}
-          wrap="soft"
+      <div>
+        <img src={item.photo} alt="" />
+      </div>
+      <div>
+        <h2>{item.title}</h2>
+        <p>{item.content}</p>
+        <form
+          className={styles.form}
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
           name="feedback"
-          required
-        ></textarea>
-        <button className={styles.button} type="submit">
-          Submit
-        </button>
-      </form>
+          method="POST"
+          action="/success"
+          id="contact-us"
+        >
+          <input type="hidden" name="form-name" value="feedback" />
+          <p className={styles.hidden}>
+            <label>
+              Don’t fill this out if you’re human: <input name="bot-field" />
+            </label>
+          </p>
+
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            className={styles.formField}
+            type="text"
+            name="name"
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            className={styles.formField}
+            type="email"
+            name="email"
+            required
+          />
+
+          <label htmlFor="feedback">What is your feedback?</label>
+          <textarea
+            id="feedback"
+            className={styles.formField}
+            wrap="soft"
+            name="feedback"
+            required
+          ></textarea>
+          <button className={styles.button} type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
--- components/Marquee.js
+++ components/Marquee.js
@@ -1,18 +1,52 @@
 import styles from './Marquee.module.scss';
 import { Logo } from './Logo';
 import { Button } from '@components/Button';
-import Link from 'next/link';
 
 export function Marquee({ item }) {
   return (
     <section className={styles.Marquee}>
-      <div className={styles.MarqueeContent}>
-        <span className={styles.Logo}>
-          <Logo />
-        </span>
-        <h1>{item.title}</h1>
-        <h2>{item.subtitle}</h2>
-        <div className={styles.buttonContainer}>
+      <div className={styles.MarqueeMain}>
+        <div className={styles.MarqueeContent}>
+          <span className={styles.Logo}>
+            <Logo />
+          </span>
+          <h1>{item.title}</h1>
+          <p>{item.subtitle}</p>
+          <div
+            className={`${styles.buttonContainer} ${styles.buttonContainerDesktop}`}
+          >
+            {item.buttons.map((button, index) => (
+              <Button
+                pointTo={button.link}
+                key={index}
+                design={index == 0 ? 'primary' : 'secondary'}
+              >
+                {button.btntitle}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className={styles.MarqueeImage}>
+          <svg
+            viewBox="0 0 737 469"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+          >
+            <defs>
+              <title>Marquee image</title>
+              <clipPath id="marquee-shape">
+                <path d="M725.779,220.575C666.707,392.517 521.11,496.962 320.681,462.416C122.125,428.201 0,342.413 0,220.575C0,98.754 162.479,0 362.89,0C563.31,0 790.425,32.444 725.779,220.575" />
+              </clipPath>
+            </defs>
+            <image
+              clipPath="url(#marquee-shape)"
+              xlinkHref={item.photo}
+            ></image>
+          </svg>
+        </div>
+        <div
+          className={`${styles.buttonContainer} ${styles.buttonContainerMobile}`}
+        >
           {item.buttons.map((button, index) => (
             <Button
               pointTo={button.link}
@@ -24,20 +58,10 @@ export function Marquee({ item }) {
           ))}
         </div>
       </div>
-      <div className={styles.MarqueeImage}>
-        <svg
-          viewBox="0 0 737 469"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <defs>
-            <title>Marquee image</title>
-            <clipPath id="marquee-shape">
-              <path d="M725.779,220.575C666.707,392.517 521.11,496.962 320.681,462.416C122.125,428.201 0,342.413 0,220.575C0,98.754 162.479,0 362.89,0C563.31,0 790.425,32.444 725.779,220.575" />
-            </clipPath>
-          </defs>
-          <image clipPath="url(#marquee-shape)" xlinkHref={item.photo}></image>
-        </svg>
+      <div className={styles.iconSet}>
+        <div>Icon</div>
+        <div>Icon</div>
+        <div>Icon</div>
       </div>
     </section>
   );
