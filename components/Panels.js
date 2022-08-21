647aba83a7c7607c0feceea8d97e78aaa1abb695
Emily Terranova <emilyeatscode@gmail.com>
2022-08-21 17:25:52 -0400

    Fix link text on panels

--- components/Panels.js
+++ components/Panels.js
@@ -4,17 +4,17 @@ export function Panels({ item }) {
       <article>
         <h3>{item.title1}</h3>
         <p>{item.paragraph1}</p>
-        {item.link1 && <a href={item.link1}>{item.link1}</a>}
+        {item.link1 && <a href={item.link1}>Read more</a>}
       </article>
       <article>
         <h3>{item.title2}</h3>
         <p>{item.paragraph2}</p>
-        {item.link2 && <a href={item.link2}>{item.link2}</a>}
+        {item.link2 && <a href={item.link2}>Read more</a>}
       </article>
       <article>
         <h3>{item.title3}</h3>
         <p>{item.paragraph3}</p>
-        {item.link3 && <a href={item.link3}>{item.link3}</a>}
+        {item.link3 && <a href={item.link3}>Read more</a>}
       </article>
     </section>
   );
