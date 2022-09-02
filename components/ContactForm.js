4c05060eac807a5fef79c798f744253abca232a8
Emily Terranova <emilyeatscode@gmail.com>
2022-09-02 08:42:25 -0400

    Add phone field and reorganize contactform

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -2,59 +2,48 @@ import styles from './ContactForm.module.scss';
 
 export function ContactForm({ item }) {
   return (
-    <section className={styles.container}>
-      <div>
-        <img src={item.photo} alt="" />
-      </div>
-      <div>
-        <h2>{item.title}</h2>
-        <p>{item.content}</p>
-        <form
-          className={styles.form}
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          name="feedback"
-          method="POST"
-          action="/success"
-          id="contact-us"
-        >
-          <input type="hidden" name="form-name" value="feedback" />
-          <p className={styles.hidden}>
-            <label>
-              Don’t fill this out if you’re human: <input name="bot-field" />
-            </label>
-          </p>
+    <form
+      className={styles.form}
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      name="feedback"
+      method="POST"
+      action="/success"
+      id="contact-us"
+    >
+      <input type="hidden" name="form-name" value="feedback" />
+      <p className={styles.hidden}>
+        <label>
+          Don’t fill this out if you’re human: <input name="bot-field" />
+        </label>
+      </p>
 
-          <label htmlFor="name">Name</label>
-          <input
-            id="name"
-            className={styles.formField}
-            type="text"
-            name="name"
-          />
+      <label htmlFor="name">Name</label>
+      <input id="name" className={styles.formField} type="text" name="name" />
 
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            className={styles.formField}
-            type="email"
-            name="email"
-            required
-          />
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        className={styles.formField}
+        type="email"
+        name="email"
+        required
+      />
 
-          <label htmlFor="feedback">What is your feedback?</label>
-          <textarea
-            id="feedback"
-            className={styles.formField}
-            wrap="soft"
-            name="feedback"
-            required
-          ></textarea>
-          <button className={styles.button} type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </section>
+      <label htmlFor="phone">Phone</label>
+      <input id="phone" className={styles.formField} type="tel" name="phone" />
+
+      <label htmlFor="feedback">Message</label>
+      <textarea
+        id="feedback"
+        className={styles.formField}
+        wrap="soft"
+        name="feedback"
+        required
+      ></textarea>
+      <button className={styles.button} type="submit">
+        Send
+      </button>
+    </form>
   );
 }
