9799eb1ef8399bfcfd89f5ba6b5223be1ed6e764
Robotnicka <emily@nimblelight.com>
2022-09-10 19:00:30 -0400

    Render markdown to html, update content module styles

--- components/Content.js
+++ components/Content.js
@@ -1,5 +1,11 @@
 import styles from './Content.module.scss';
+import md from 'markdown-it';
 
 export function Content({ item }) {
-  return <main className={styles.Content}>{item.content}</main>;
+  return (
+    <main className={styles.Content}>
+      <div dangerouslySetInnerHTML={{ __html: md().render(item.content) }} />
+      {console.log(item.content)}
+    </main>
+  );
 }
