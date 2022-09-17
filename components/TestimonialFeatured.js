7e1a0a3eb1c31753fc17325f6de314b9b21ae786
Robotnicka <emily@nimblelight.com>
2022-09-17 10:30:09 -0400

    Featured testimonial styles

--- components/TestimonialFeatured.js
+++ components/TestimonialFeatured.js
@@ -1,15 +1,22 @@
 import { Button } from './Button';
 import { Testimonial } from './parts/Testimonial';
+import styles from './TestimonialFeatured.module.scss';
+import Quote from '../assets/svg/quote.svg';
 
 export function TestimonialFeatured({ item }) {
   return (
-    <section>
-      <h3>Testimonials</h3>
-      <figure className={style.Quote}>
-        <blockquote className={styles.QuoteContainer}>
+    <section className={styles.testimonialFeatured}>
+      <h2 className={styles.header}>Testimonials</h2>
+      <figure className={styles.quoteContainer}>
+        <span className={styles.icon}>
+          <Quote />
+        </span>
+        <blockquote className={styles.quote}>
           <p>{item.quote}</p>
         </blockquote>
-        {item.author && <figcaption>— {item.author}</figcaption>}
+        {item.author && (
+          <figcaption className={styles.author}>— {item.author}</figcaption>
+        )}
       </figure>
       <Button pointTo={'/Testimonials'} design="secondary">
         See more reviews
