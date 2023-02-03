29654d41c3ac28c2e200e256f9d4f981f4e2ae7b
Robotnicka <emily@nimblelight.com>
2023-02-03 12:06:41 -0500

    Remove script

--- pages/_document.js
+++ pages/_document.js
@@ -24,17 +24,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script>
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            });
-          }
-        </script>
       </body>
     </Html>
   );
