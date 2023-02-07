ef3cdcffbbb8d5ebe1b023ea7f45c20a3b8c0b64
Emily Terranova <emily@nimblelight.com>
2023-02-07 14:32:50 -0500

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -18,8 +18,6 @@ const AdminPage = () => {
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-      <div data-netlify-identity-menu></div>
-      <div data-netlify-identity-button>Login with Netlify Identity</div>
       <CMS />
     </>
   );
