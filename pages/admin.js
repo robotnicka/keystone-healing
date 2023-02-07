b64a613d82ba628914d3fd3e3481cdf2e5059884
Emily Terranova <emily@nimblelight.com>
2023-02-07 14:08:46 -0500

    Test admin

--- pages/admin.js
+++ pages/admin.js
@@ -17,6 +17,9 @@ const AdminPage = () => {
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
+      <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
+      <div data-netlify-identity-menu></div>
+      <div data-netlify-identity-button>Login with Netlify Identity</div>
       <CMS />
     </>
   );
